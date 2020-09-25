bd708243c288820bf0fd92aa846d0c02b84aab80
Patric Gutersohn <pguso@gmx.de>
2020-09-25 13:55:43 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {User, UserService} from 'slim-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-openapi-client-demo';
+export class AppComponent implements OnInit {
+  users$: Observable<User[]>;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.users$ = this.userService.getUsers();
+  }
 }
